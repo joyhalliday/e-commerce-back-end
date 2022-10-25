f8ed3b47bd96fa23d2de6ecb5da3d959baa69daf
Joy Halliday <tjoyhalliday@gmail.com>
2022-10-24 18:27:03 -0700

    updated category routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -17,8 +17,18 @@ router.post('/', (req, res) => {
   // create a new tag
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+try {
+  const tagData = await Tag.update(req.params.id);
+  if (!tagData) {
+    res.status(404).json({ message: 'No product with this id.' });
+    return;
+  }
+  res.status(200).json(tagData);
+} catch (err) {
+  res.status(500).json(err);
+}
 });
 
 router.delete('/:id', (req, res) => {
